8de21eddb06b4649625aa9110b3fd92572751879
jesusxal777-boop <jesusxal777@gmail.com>
2026-06-01 01:51:49 -0500

    Refactor authentication flow and token handling

--- js/auth.js
+++ js/auth.js
@@ -1,25 +1,65 @@
+const CLIENT_ID =
+"743766102684-320nu8h0d5dnoac0mpdg1stm582nmb6k.apps.googleusercontent.com";
+
+const SCOPES =
+"https://www.googleapis.com/auth/documents https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email";
+
+let tokenClient;
 let accessToken = null;
 
-function initAuth() {
+window.onload = () => {
 
-    google.accounts.id.initialize({
-        client_id:
-        "743766102684-320nu8h0d5dnoac0mpdg1stm582nmb6k.apps.googleusercontent.com",
-        callback: handleCredentialResponse
+    tokenClient = google.accounts.oauth2.initTokenClient({
+        client_id: CLIENT_ID,
+        scope: SCOPES,
+        callback: async (response) => {
+
+            accessToken = response.access_token;
+
+            await loadProfile();
+
+            loadDocuments();
+
+        }
     });
 
-}
+};
 
-function handleCredentialResponse(response){
+document
+.getElementById("login-btn")
+.addEventListener("click", () => {
 
-    console.log(response);
+    tokenClient.requestAccessToken();
+
+});
+
+async function loadProfile(){
+
+    const res = await fetch(
+        "https://www.googleapis.com/oauth2/v2/userinfo",
+        {
+            headers:{
+                Authorization:`Bearer ${accessToken}`
+            }
+        }
+    );
+
+    const user = await res.json();
+
+    document
+    .getElementById("username")
+    .textContent = user.name;
+
+    document
+    .getElementById("avatar")
+    .src = user.picture;
+
+    document
+    .getElementById("user-info")
+    .hidden = false;
 
     document
     .getElementById("login-btn")
     .style.display = "none";
 
 }
-
-document
-.getElementById("login-btn")
-.addEventListener("click",initAuth);
